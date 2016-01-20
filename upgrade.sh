97c25c2a70022ca703c91e2805e09ca8fa705c90
Wahlstedt, Walter <wwahlstedt@unionky.edu>
2016-01-20 13:29:43 -0500

    work on branch question, backups and restore

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,9 @@
-#  This is a script to upgrade snipeit
-#  Written by: Walter Wahlstedt
+#!/bin/bash
 
+#  This is a script to upgrade snipeit
+#  Written by: Walter Wahlstedt (merid14)
+
+set -e
 # ensure running as root
 if [ "$(id -u)" != "0" ]; then
   exec sudo "$0" "$@"
@@ -8,14 +11,15 @@ fi
 
 clear
 #  Set this to your github username to pull your changes ** Only for Devs **
-fork='snipe'
-#  Set this to the branch you want to pull  ** Only for Devs **
-branch='develop'
+fork="snipe"
+#  Set this to the branch you want to pull  ** Only for Devs ** ##TODO not working yet
+#branch="develop"
 
 ##TODO: Update docs on what the upgrade script is doing
 
 name='snipeit'
 si="Snipe-IT"
+setbranch=1
 date="$(date '+%Y-%b-%d')"
 backup=/opt/$name/backup/$date
 webdir=/var/www/html
@@ -27,70 +31,80 @@ echo ""
 
 
 ##TODO: Check if /var/log/snipeit-install.log exists. if it does suggest upgrade path.
-##TODO: Add .snipeitinstaller with app version
+##TODO: Add .installed with app version
 
-if [$log]
+if [!$log] || [!$installed]
 then
-    echo "##  $name install found."
-    echo "    Proceeding with upgrade."
-    echo >> $installed "updated to $si version: from:"
+    echo "    It appears that you haven't installed $name with the installer."
+    echo "    Please upgrade manually by following the instructions in the documentation."
+    echo "    http://docs.snipeitapp.com/upgrading.html"
+##TODO: ask if user would like to procceed anyway and promp them to backup their files.
 else
+    echo "##  $name install found."
+    echo "##  Beginning the $si update process."
+    echo ""
+    echo >> $installed "Upgraded to $si version: from:"
 fi
-
-
-echo "##  Beginning the snipeit update process."
-echo ""
 
 echo "##  Setting up backup directory."
 echo "    $backup"
 mkdir $backup
 
-##TODO: Backup app/config/app.php file /opt/snipeit/backup/$date
-##TODO: Backup database to /opt/snipeit/backup/$date
-##TODO: Empty these directories: app/storage/cache and app/storage/views
-
-echo "Getting update."
+echo "##  Backing up app file."
 echo ""
+cp $webdir/$name/app/config/app.php $backup/app.php
 
-#TODO: Write warning that we are pulling from master not the latest release.
-    #TODO: Pull from latest release with: git tag | grep -v 'pre' | tail -1
+echo "##  Backing up database."
+echo ""
+mysqldump db_name > $backup/$name.sql
+
+echo "##  Getting update."
+echo ""
+# echo "    By default we are pulling from the latest release not master."
+# echo "    If you pulled from "
+# echo ""
 
 until [[ $ans == "yes" ]]; do
-setbranch=1
-echo "  Q. What branch would you like to use to upgrade? (latest release) "
-echo ""
-echo "    1. Latest Release (default)"
-echo "    2. Master"
-echo "    3. Develop"
-echo ""
-read setbranch
-echo " setbranch equals $setbranch."
-case $setbranch in
+    echo "  Q. What branch would you like to use to upgrade?"
+    echo ""
+    echo "    1. Latest Release (default)"
+    echo "    2. Master"
+    echo "    3. Develop"
+    echo ""
+    read setbranch
+    echo " setbranch equals $setbranch." ## for debug
+    case $setbranch in
         1)
-              echo "  Branch has been set to latest release."
-
-                branch=$(git tag | grep -v 'pre' | tail -1)
-                ans="yes"
-                ;;
+            branch=$(git tag | grep -v 'pre' | tail -1)
+            echo "  Branch has been set to latest release. $branch"
+            ans="yes"
+            ;;
         2)
-              echo "  Branch has been set to master."
-                branch="master"
-                ans="yes"
-                ;;
+            branch="master"
+            echo "  Branch has been set to $branch."
+            ans="yes"
+            ;;
         3)
-                echo "  Branch has been set to develop."
-                branch="develop"
-                ans="yes"
-                ;;
-        *)        echo "  Invalid answer. Please select the number for the branch you want."
-                ;;
-esac
+            branch="develop"
+            echo "  Branch has been set to $branch."
+            ans="yes"
+            ;;
+        *)
+            echo "  Invalid answer. Please select the number for the branch you want."
+            ;;
+    esac
 done
-file=$branch'.zip'
 
-# echo "Applying update files."
-##TODO: Copy app/config/app.php back
+git pull
 
+
+echo "##  Cleaning cache and view directories."
+echo ""
+rm -rf $webdir/$name/app/storage/cache/*
+rm -rf $webdir/$name/app/storage/views/*
+
+echo "##  Restoring app.php file."
+cp $backup/app.php $webdir/$name/app/config/app.php
 
 echo "##  Running composer to apply update."
 echo ""
