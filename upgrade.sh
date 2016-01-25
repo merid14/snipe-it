e6865b90f9e2fe519a249974a01b57601ae06459
Wahlstedt, Walter <wwahlstedt@unionky.edu>
2016-01-25 14:04:30 -0500

    formatting

--- upgrade.sh
+++ upgrade.sh
@@ -40,25 +40,7 @@ else
     echo "##  Beginning the $si update process."
     echo ""
 
-echo "##  Setting up backup directory."
-echo "    $backup"
-mkdir -p $backup
-
-echo "##  Backing up app file."
-echo ""
-cp $webdir/$name/app/config/app.php $backup/app.php
-
-echo "##  Backing up database."
-echo ""
-mysqldump $name > $backup/$name.sql
-
-echo "##  Getting update."
-echo ""
-echo "    By default we are pulling from the latest release."
-echo "    If you pulled from another branch please upgrade manually."
-echo ""
-cd $webdir/$name
-
+    cd $webdir/$name
 # until [[ $ans == "yes" ]]; do
 #     echo "  Q. What branch would you like to use to upgrade?"
 #     echo ""
@@ -95,37 +77,74 @@ cd $webdir/$name
 #             ;;
 #     esac
 # done
-if [ -z $branch ]
-then
-    branch=$(git tag | grep -v 'pre' | tail -1)
-fi
+    if [ -z $branch ]
+    then
+        branch=$(git tag | grep -v 'pre' | tail -1)
+    fi
+    currentBranch=$(basename $(git symbolic-ref HEAD))
 
-currentBranch=$(basename $(git symbolic-ref HEAD))
+    if [ $currentBranch != $branch ]
+    then
+        echo "##  Setting up backup directory."
+        echo "    $backup"
+        mkdir -p $backup
 
-if [ $currentBranch=$branch ]
-then
-    echo "    You are already on the latest version."
-    echo "    Version: $currentBranch"
-else
-    git add .
-    git commit -m "Upgrading"
-    git stash
-    git checkout -b $branch $branch
-    git stash pop
-    echo >> $installed "Upgraded $si to version:$branch from:$currentBranch"
-fi
+        echo "##  Backing up app file."
+        echo ""
+        cp $webdir/$name/app/config/app.php $backup/app.php
 
-echo "##  Cleaning cache and view directories."
-echo ""
-rm -rf $webdir/$name/app/storage/cache/*
-rm -rf $webdir/$name/app/storage/views/*
+        echo "##  Backing up database."
+        echo ""
+        mysqldump $name > $backup/$name.sql
 
-echo "##  Restoring app.php file."
-cp $backup/app.php $webdir/$name/app/config/app.php
+        echo "##  Getting update."
+        echo ""
+        echo "    By default we are pulling from the latest release."
+        echo "    If you pulled from another branch please upgrade manually."
+        echo ""
 
-echo "##  Running composer to apply update."
-echo ""
-sudo php composer.phar install --no-dev --prefer-source
-sudo php composer.phar dump-autoload
-sudo php artisan migrate
+        until [[ $ans1 == "yes" ]]; do
+        echo -n "  Q. Would you like to continue? (y/n) "
+        read cont
+        case $cont in
+                [yY] | [yY][Ee][Ss] )
+                        echo "  Continuing with the upgrade process."
+                        ans1="yes"
+                        ;;
+                [nN] | [n|N][O|o] )
+                        echo "  Exiting now!"
+                        exit
+                        ;;
+                *)      echo "    Invalid answer. Please type y or n"
+                        ;;
+        esac
+        done
+
+        git add .
+        git commit -m "Upgrading"
+        git stash
+        git checkout -b $branch $branch
+        git stash pop
+
+        echo "##  Cleaning cache and view directories."
+        echo ""
+        rm -rf $webdir/$name/app/storage/cache/*
+        rm -rf $webdir/$name/app/storage/views/*
+
+        echo "##  Restoring app.php file."
+        cp $backup/app.php $webdir/$name/app/config/app.php
+
+        echo "##  Running composer to apply update."
+        echo ""
+        sudo php composer.phar install --no-dev --prefer-source
+        sudo php composer.phar dump-autoload
+        sudo php artisan migrate
+        echo >> $installed "Upgraded $si to version:$branch from:$currentBranch"
+    else
+        echo "    You are already on the latest version."
+        echo "    Version: $currentBranch"
+        echo ""
+    fi
+
+
 fi
