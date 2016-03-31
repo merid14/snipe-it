147e72d86d632a249348460c20b9a34b90ddc014
merid14 <merid14@gmail.com>
2016-03-31 11:26:48 -0400

    lets try this again

--- install/snipeit.sh
+++ install/snipeit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=
+# shellcheck disable=SC2154,SC2034
 # -----------------------------------------------------------------------------
 #   Snipe-It Installer
 #   Mike Tucker | mtucker6784@gmail.com
@@ -16,18 +16,14 @@
 if [ "$(id -u)" != "0" ]; then
   exec sudo "$0" "$@"
 fi
-. /install/functions.sh
+#include functions
+. "$tmpinstall"/functions.sh
 
 clear
-log="/var/log/snipeit-install.log"
+
 
 echo "--------------  Collect info for log  -----------------" >> "$log" 2>&1
 getOSinfo
-
-#  Set this to your github username to pull your changes ** Only for Devs **
-logvar fork="snipe"
-#  Set this to the branch you want to pull  ** Only for Devs **
-logvar branch=""
 
 echo "--------------  Declare Variables  -----------------" >> "$log" 2>&1
 logvar distro="$os"
@@ -35,6 +31,7 @@ logvar os="OS: $os"
 logvar arch="Arch: $arch"
 logvar kernel="Kernel: $kernel"
 logvar supportedos="Redhat/CentOS 6+ and Debian/Ubuntu 10.04+"
+logvar log="/var/log/snipeit-install.log"
 logvar name="snipeit"
 logvar si="Snipe-IT"
 logvar hostname="$(hostname)"
@@ -43,14 +40,13 @@ logvar installed="$webdir/$name/.installed"
 logvar tmp=/tmp/"$name"
 logvar date="$(date '+%Y-%b-%d')"
 logvar backup=/opt/"$name"/backup/"$date"
-
 logvar newBranch="$branch"
 logvar appkey="$(< /dev/urandom tr -dc _A-Za-z-0-9 | head -c32)"
-logvar dbsetup=$tmp/db_setup.sql
+logvar dbsetup="$tmp"/db_setup.sql
 logvar mariadbRepo=/etc/yum.repos.d/MariaDB.repo
 
 webdir=""
-apachefile""
+apachefile=""
 tzone=""
 apacheuser=""
 apachelog=""
@@ -61,15 +57,15 @@ echo "--------------  Start Installer  -----------------" >> "$log" 2>&1
 showBanner
 
 shopt -s nocasematch
-case $distro in
+case "$distro" in
     *Ubuntu*|*Debian*)
         echo "  The installer has detected Ubuntu/Debian as the OS."
         logvar distro=ubuntu
         logvar webdir=/var/www
         logvar installed="$webdir/$name/.installed"
         logvar gitdir="$webdir/$name/.git"
-        logvar apachefile=/etc/apache2/sites-available/$name.conf
-        logvar tzone=$(cat /etc/timezone);
+        logvar apachefile=/etc/apache2/sites-available/"$name".conf
+        logvar tzone="$(cat /etc/timezone)";
         logvar apacheuser="www-data:www-data"
         logvar apachelog="/var/log/apache2"
         ;;
@@ -79,8 +75,8 @@ case $distro in
         logvar webdir=/var/www/html
         logvar installed="$webdir/$name/.installed"
         logvar gitdir="$webdir/$name/.git"
-        logvar apachefile=/etc/httpd/conf.d/$name.conf
-        logvar tzone=$(grep ZONE /etc/sysconfig/clock | tr -d '"' | sed 's/ZONE=//g');
+        logvar apachefile=/etc/httpd/conf.d/"$name".conf
+        logvar tzone="$(grep ZONE /etc/sysconfig/clock | tr -d '"' | sed 's/ZONE=//g')";
         logvar apacheuser="apache:apache"
         logvar apachelog="/var/log/httpd"
         ;;
@@ -88,8 +84,8 @@ case $distro in
         echo "  The installer has detected redhat/centos 7 as the OS."
         logvar distro=centos7
         logvar webdir=/var/www/html
-        logvar apachefile=/etc/httpd/conf.d/$name.conf
-        logvar tzone=$(timedatectl | gawk -F'[: ]+' ' $2 ~ /Timezone/ {print $3}');
+        logvar apachefile=/etc/httpd/conf.d/"$name".conf
+        logvar tzone="$(timedatectl | gawk -F'[: ]+' ' $2 ~ /Timezone/ {print $3}')";
         logvar apacheuser="apache:apache"
         logvar apachelog="/var/log/httpd"
         ;;
@@ -103,15 +99,13 @@ case $distro in
         ;;
 esac
 
-rm -rf "${$tmp:?}/"
-mkdir "$tmp"
-
 UpgradeSnipeit
 
 askFQDN
 askDBuserpw
 
-case $distro in
+shopt -s nocasematch
+case "$distro" in
     debian|ubuntu)
         setupPackages
         startApache
@@ -144,7 +138,7 @@ esac
 echo "##  Cleaning up..."
 rm -rf "${tmp:?}/"
 echo
-echo >> $installed "Installed $si to version:$branch $date"
+echo >> "$installed" "Installed $si to version:$branch $date"
 echo
 echo -e "\e[31m The mail configuration has not been setup.\e[0m"
 echo -e "\e[31m   To setup follow the docs here:\e[0m"
