464f1f782d54b9f7e66033d9ead414c07e4f4bde
merid14 <merid14@gmail.com>
2016-04-01 13:07:24 -0400

    add read for testings

--- install.sh
+++ install.sh
@@ -10,7 +10,13 @@ branch="master"
 link="https://raw.githubusercontent.com/$fork/snipe-it/$branch/install/"
 tmp=/tmp/snipeit/
 tmpinstall=/tmp/snipeit/install/
+islog="false"
 log="/var/log/snipeit-install.log"
+if [ -f "$log" ];then
+    echo "log is already here: $log"
+    islog="true"
+fi
+
 
 rm -rf "${tmp:?}"
 mkdir -p "$tmpinstall"
@@ -18,6 +24,6 @@ mkdir -p "$tmpinstall"
 wget "$link"/snipeit.sh -P "$tmpinstall" 2>&1 | grep -i "failed\|error"
 wget "$link"/upgrade.sh -P "$tmpinstall" 2>&1 | grep -i "failed\|error"
 wget "$link"/functions.sh -P "$tmpinstall" 2>&1 | grep -i "failed\|error"
-
+read test
 chmod -R 755 "$tmpinstall"
 . "$tmpinstall"/snipeit.sh 2>&1 | sudo tee -a /var/log/snipeit-install.log
