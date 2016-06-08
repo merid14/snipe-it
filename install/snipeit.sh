27c0ce54f02410c171b695b0439711ba7c7007e8
merid14 <merid14@gmail.com>
2016-06-08 09:18:21 -0400

    move to install folder

---
+++ install/snipeit.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+# shellcheck disable=SC2154,SC2034
+# -----------------------------------------------------------------------------
+#   Snipe-It Installer
+#   Mike Tucker | mtucker6784@gmail.com
+#   Walter Wahlstedt | merid14@gmail.com
+#
+#   This installer is for Debian and CentOS based distributions.
+#   We assume you will be installing as a subdomain on a fresh OS install.
+#   Mail is setup separately. SELinux is assumed to be disabled
+#
+#   Feel free to modify, but please givecredit where it's due. Thanks!
+# -----------------------------------------------------------------------------
+set -o nounset errexit pipefail
+
+# ensure running as root
+if [ "$(id -u)" != "0" ]; then
+  exec sudo "$0" "$@"
+fi
+#include functions
+tmp=/tmp/snipeit && echo "$tmp" >> "$log" 2>&1
+tmpinstall=/tmp/snipeit/install/ && echo "$tmpinstall" >> "$log" 2>&1
+. "$tmpinstall"/functions.sh
+clear
+
+
+echo "--------------  Collect info for log  -----------------" >> "$log" 2>&1
+getOSinfo
+
+echo "--------------  Declare Variables  -----------------" >> "$log" 2>&1
+
+name="snipeit"
+si="Snipe-IT"
+webdir=""
+apachefile=""
+tzone=""
+apacheuser=""
+apachelog=""
+apacheversion=""
+
+distro="$os" && echo "$distro" >> "$log" 2>&1
+os="OS: $os" && echo "$os" >> "$log" 2>&1
+arch="Arch: $arch" && echo "$arch" >> "$log" 2>&1
+kernel="Kernel: $kernel" && echo "$kernel" >> "$log" 2>&1
+supportedos="Redhat/CentOS 6+ and Debian/Ubuntu 10.04+" && echo "$supportedos" >> "$log" 2>&1
+log="/var/log/snipeit-install.log" && echo "$log" >> "$log" 2>&1
+hostname="$(hostname)" && echo "$hostname" >> "$log" 2>&1
+fqdn="$(hostname --fqdn)" && echo "$fqdn" >> "$log" 2>&1
+installed="$webdir/$name/.installed" && echo "$installed" >> "$log" 2>&1
+tmp=/tmp/"$name" && echo "$tmp" >> "$tmp" 2>&1
+tmpinstall=/tmp/snipeit/install/ && echo "$tmpinstall" >> "$log" 2>&1
+date="$(date '+%Y-%b-%d')" && echo "$date" >> "$log" 2>&1
+backup=/opt/"$name"/backup/"$date" && echo "$backup" >> "$log" 2>&1
+newBranch="$branch" && echo "$newBranch" >> "$log" 2>&1
+appkey="$(< /dev/urandom tr -dc _A-Za-z-0-9 | head -c32)" && echo "$appkey" >> "$log" 2>&1
+dbsetup="$tmp"/db_setup.sql && echo "$dbsetup" >> "$log" 2>&1
+mariadbRepo=/etc/yum.repos.d/MariaDB.repo && echo "$mariadbRepo" >> "$log" 2>&1
+
+echo "--------------  Start Installer  -----------------" >> "$log" 2>&1
+showBanner
+
+shopt -s nocasematch
+case "$distro" in
+    *Ubuntu*|*Debian*)
+        echo "  The installer has detected Ubuntu/Debian as the OS."
+        distro=ubuntu && echo "$distro" >> "$log" 2>&1
+        webdir=/var/www && echo "$webdir" >> "$log" 2>&1
+        installed="$webdir/$name/.installed" && echo "$installed" >> "$log" 2>&1
+        gitdir="$webdir/$name/.git" && echo "$gitdir" >> "$log" 2>&1
+        apachefile=/etc/apache2/sites-available/"$name".conf && echo "$apachefile" >> "$log" 2>&1
+        tzone="$(cat /etc/timezone)" && echo "$tzone" >> "$log" 2>&1
+        apacheuser="www-data:www-data" && echo "$apacheuser" >> "$log" 2>&1
+        apachelog="/var/log/apache2" && echo "$apachelog" >> "$log" 2>&1
+        ;;
+    *centos*6*|*redhat*6*)
+        echo "  The installer has detected redhat/centos 6 as the OS."
+        distro=centos6 && echo "$distro" >> "$log" 2>&1
+        webdir=/var/www/html && echo "$webdir" >> "$log" 2>&1
+        installed="$webdir/$name/.installed" && echo "$installed" >> "$log" 2>&1
+        gitdir="$webdir/$name/.git" && echo "$gitdir" >> "$log" 2>&1
+        apachefile=/etc/httpd/conf.d/"$name".conf && echo "$apachefile" >> "$log" 2>&1
+        tzone="$(grep ZONE /etc/sysconfig/clock | tr -d '"' | sed 's/ZONE=//g')" && echo "$tzone" >> "$log" 2>&1
+        apacheuser="apache:apache" && echo "$apacheuser" >> "$log" 2>&1
+        apachelog="/var/log/httpd" && echo "$apachelog" >> "$log" 2>&1
+        ;;
+    *centos*7*|*redhat*7*)
+        echo "  The installer has detected redhat/centos 7 as the OS."
+        distro=centos7 && echo "$distro" >> "$log" 2>&1
+        webdir=/var/www/html && echo "$webdir" >> "$log" 2>&1
+        installed="$webdir/$name/.installed" && echo "$installed" >> "$log" 2>&1
+        gitdir="$webdir/$name/.git" && echo "$gitdir" >> "$log" 2>&1
+        apachefile=/etc/httpd/conf.d/"$name".conf && echo "$apachefile" >> "$log" 2>&1
+        tzone="$(timedatectl | gawk -F'[: ]+' ' $2 ~ /Timezone/ {print $3}')" && echo "$tzone" >> "$log" 2>&1
+        apacheuser="apache:apache" && echo "$apacheuser" >> "$log" 2>&1
+        apachelog="/var/log/httpd" && echo "$apachelog" >> "$log" 2>&1
+        ;;
+    *)
+        echo -e "\e[31m  The installer has detected $distro as the OS.\e[0m"
+        echo -e "\e[31m  Unfortunately this installer doesn't work on your os.\e[0m"
+        echo -e "\e[31m  Supported OS's are: $supportedos\e[0m"
+        echo -e "\e[31m  Please see snipeit docs for manual install: .\e[0m"
+        echo -e "\e[31m      http://docs.snipeitapp.com/installation/downloading.html.\e[0m"
+        exit
+        ;;
+esac
+
+UpgradeSnipeit
+
+askFQDN
+askDBuserpw
+
+shopt -s nocasematch
+case "$distro" in
+    debian|ubuntu)
+        setupPackages
+        startApache
+        setupGitSnipeit
+        setupApacheMods
+        setupApacheHost
+        setupFiles
+        setupDB
+        setupComposer
+        setupPermissions
+        setupSnipeit
+        startApache
+        ;;
+    centos* )
+        setupRepos
+        setupPackages
+        setupGitSnipeit
+        setupDB
+        setupApacheHost
+        startApache
+        setupFiles
+        setupComposer
+        setupPermissions
+        setupSnipeit
+        ##setupSELinux
+        startApache
+        ;;
+esac
+
+echo "##  Cleaning up..."
+rm -rf "${tmp:?}/"
+echo
+echo >> "$installed" "Installed $si to version:$branch $date"
+echo
+echo -e "\e[31m The mail configuration has not been setup.\e[0m"
+echo -e "\e[31m   To setup follow the docs here:\e[0m"
+echo "   http://docs.snipeitapp.com/installation/configuration.html"
+echo
+echo -e "\e[31m  SELinux has not been configured. Please follow the docs here:\e[0m"
+echo "     http://docs.snipeitapp.com/installation/server/linux-osx.html"
+echo
+echo
+echo "  ***Open http://$fqdn to login to Snipe-IT.***"
---
+++ install/upgrade.sh
@@ -0,0 +1,225 @@
+#!/bin/bash
+# shellcheck disable=SC2154,SC2034
+#  This is a script to upgrade snipeit
+#  Written by: Walter Wahlstedt (merid14)
+
+
+ans=""
+cd "$webdir"/"$name" || exit
+echo "##  Checking for  previous version of $si."
+echo
+
+if [ -f "$log" ] || [ -f "$installed" ]; then #If log or installer file exists
+    if [ -d "$gitdir" ]; then # If git directory exists
+        if [ -z "$newBranch" ]; then # If newBranch is empty then get the latest release
+            newBranch=$(git tag | grep -v 'pre' | tail -1)
+        fi
+        currentBranch="$(basename "$(git symbolic-ref HEAD)")"
+
+        echo "##  $si install found. Version: $currentBranch"
+
+        if compareVersions "$currentBranch" "$newBranch"; then ##TODO Strip "v" from version name to allow number calculation
+
+            echo "##  Beginning the $si update process to version: $newBranch"
+            echo ""
+            echo "    By default we are pulling from the latest release."
+            echo "    If you pulled from another branch please upgrade manually."
+            echo
+
+            until [[ $ans == "yes" ]]; do
+                echo "##  Upgrading to Version: $newBranch from Version: $currentBranch"
+                echo ""
+                echo -n "  Q. Would you like to continue? (y/n) "
+                read -r cont
+                shopt -s nocasematch
+                case $cont in
+                    y | yes )
+                        echo "  Continuing with the upgrade process to version: $newBranch."
+                        echo
+                        ans="yes"
+                        ;;
+                    n | no )
+                        echo "  Exiting now!"
+                        exit
+                        ;;
+                    *)
+                        echo "    Invalid answer. Please type y or n"
+                        ;;
+                esac
+            done
+
+            if [ -d "$backup" ]; then #if dir doesnt exist
+                echo "##  Backup directory already exists, using it."
+                echo "    $backup"
+            else
+                echo "##  Setting up backup directory."
+                echo "    $backup"
+                echo
+                mkdir -p "$backup"
+            fi
+
+            echo "##  Backing up app file."
+            echo
+            cp -p "$webdir"/"$name"/app/config/app.php "$backup"/
+
+            echo "##  Backing up database."
+            echo ""
+            mysqldump "$name" > "$backup"/"$name".sql
+
+            echo "##  Getting update."
+            echo
+
+            ## run git update
+            cd "$webdir"/"$name" || exit
+            set +e
+            git add . >> "$log" 2>&1
+            git commit -m "Upgrading to $newBranch from $currentBranch" >> "$log" 2>&1
+            git stash >> "$log" 2>&1
+            git checkout -b "$newBranch" "$newBranch" >> "$log" 2>&1
+            git stash pop >> "$log" 2>&1
+            set -e
+
+            echo "##  Cleaning cache and view directories."
+            rm -rf "$webdir"/"$name"/app/storage/cache/*
+            rm -rf "$webdir"/"$name"/app/storage/views/*
+
+            # rm -rf "${$webdir:?}"/"${$name:?}"/app/storage/cache/*
+            # rm -rf "${$webdir:?}"/"${$name:?}"/app/storage/views/*
+
+
+            echo "##  ##  Restoring app.php file."
+            cp "$backup"/app.php "$webdir"/"$name"/app/config/
+
+
+        else
+            echo "    You are already on the latest version."
+            echo "    Version: $currentBranch"
+            echo
+            exit
+        fi
+    else  # Must be a file copy install
+        #get the current version
+        echo -n "##  Beginning conversion from copy file install to git install."
+        currentVersion="$(cat "$webdir"/"$name"/app/config/version.php | grep app | awk -F "'" '{print $4}' | cut -f1 -d"-")"
+
+        #clone to tmp so we can check the latest version
+        rm -rf "${tmp:?}/"
+
+        ShowProgressOf git clone https://github.com/"$fork"/snipe-it "$tmp"
+        cd "$tmp" || exit
+
+        if [ -z "$newBranch" ]; then # If newBranch is empty then get the latest release
+            newBranch=$(git tag | grep -v 'pre' | tail -1)
+        fi
+        if compareVersions "$currentVersion" "$newBranch"; then
+            until [[ $ans1 == "yes" ]]; do
+            echo "##  Upgrading to Version: $newBranch from Version: $currentVersion"
+            echo
+            echo -n "  Q. Would you like to continue? (y/n) "
+            read -r cont
+            shopt -s nocasematch
+            case $cont in
+                    y | yes )
+                        echo "    Continuing with the upgrade process to version: $newBranch."
+                        echo
+                        ans1="yes"
+                        ;;
+                    n | no )
+                        echo "  Exiting now!"
+                        exit
+                        ;;
+                    *)
+                        echo "    Invalid answer. Please type y or n"
+                        ;;
+            esac
+            done
+
+            if [ -d "$backup" ]; then #if dir does exist
+                echo "##  Backup directory already exists, using it."
+                echo "    $backup"
+            else
+                echo "##  Setting up backup directory."
+                echo "    $backup"
+                echo
+                mkdir -p "$backup"
+            fi
+
+            echo "##  Backing up app file."
+            cp "$webdir"/"$name"/app/config/app.php "$backup"
+
+            echo "##  Backing up $si folder."
+            cp -R "$webdir"/"$name" "$backup"/"$name"
+            rm -rf "${webdir:?}"/"${name:?}"
+
+            echo "##  Backing up database."
+            mysqldump "$name" > "$backup"/"$name".sql
+
+            echo -n "##  Downloading Snipe-IT from github and put it in the web directory...";
+            ShowProgressOf git clone https://github.com/"$fork"/snipe-it "$webdir"/"$name"
+            # get latest stable release
+            cd "$webdir"/"$name" || exit
+            if [ -z "$newBranch" ]; then
+                newBranch=$(git tag | grep -v 'pre' | tail -1)
+            fi
+
+            echo "    Installing version: $newBranch"
+            git checkout -b "$newBranch" "$newBranch" >> "$log" 2>&1
+
+            echo "##  Restoring files."
+            echo "      Restoring app config file."
+            if [ -e "$backup"/app.php ]; then
+                cp "$backup"/app.php "$webdir"/"$name"/app/config/
+            fi
+            echo "      Restoring app production file."
+            if [ -e "$backup"/"$name"/app/config/production/app.php ]; then
+                cp "$backup"/"$name"/app/config/production/app.php "$webdir"/"$name"/app/config/production/
+            fi
+            echo "      Restoring bootstrap file."
+            if [ -e "$backup"/"$name"/bootstrap/start.php ]; then
+                cp "$backup"/"$name"/bootstrap/start.php "$webdir"/"$name"/bootstrap/
+            fi
+            echo "      Restoring database file."
+            if [ -e "$backup"/"$name"/app/config/production/database.php ]; then
+                cp "$backup"/"$name"/app/config/production/database.php "$webdir"/"$name"/app/config/production/
+            fi
+            echo "      Restoring mail file."
+            if [ -e "$backup"/"$name"/app/config/production/mail.php ]; then
+                cp "$backup"/"$name"/app/config/production/mail.php "$webdir"/"$name"/app/config/production/
+            fi
+            if compareVersions "$currentVersion" 2.1.0; then
+                echo "      Restoring ldap file."
+                if [ -e "$backup"/"$name"/app/config/production/ldap.php ]; then
+                    cp "$backup"/"$name"/app/config/production/ldap.php "$webdir"/"$name"/app/config/production/
+                fi
+            fi
+            echo "      Restoring composer files."
+            if [ -e "$backup"/"$name"/composer.phar ]; then
+                cp "$backup"/"$name"/composer.phar "$webdir"/"$name"
+            fi
+            if [ -d "$backup"/"$name"/vendor ]; then
+                cp -r "$backup"/"$name"/vendor "$webdir"/"$name"
+            fi
+        else
+            echo "    You are already on the latest version."
+            echo "    Version: $currentBranch"
+            echo
+            exit
+        fi
+    fi
+        # Change permissions on directories
+        setupPermissions
+
+        echo "##  Running composer to apply update."
+        echo
+        php composer.phar install --no-dev --prefer-source
+        php composer.phar dump-autoload
+        php artisan migrate
+
+        echo >> "$installed" "Upgraded $si to version:$newBranch from:$currentBranch"
+
+        echo
+        echo "    You are now on Version $newBranch of $si."
+        exit
+else
+    echo "    No previous version of $si found."
+fi
