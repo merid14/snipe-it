34c47518da2bfcc9edf53e3553560cab4c24df52
merid14 <merid14@gmail.com>
2016-06-08 09:23:16 -0400

    add download of files, and set vars.

--- install.sh
+++ install.sh
@@ -2,6 +2,28 @@
 if [ "$(id -u)" != "0" ]; then
   exec sudo "$0" "$@"
 fi
-wget https://raw.githubusercontent.com/snipe/snipe-it/master/snipeit.sh
-chmod 744 snipeit.sh
-sudo ./snipeit.sh 2>&1 | sudo tee -a /var/log/snipeit-install.log
+
+#  Set this to your github username and branch to pull your changes ** Only for Devs **
+fork="snipe"
+branch="master"
+
+link="https://raw.githubusercontent.com/$fork/snipe-it/$branch/install/"
+tmp=/tmp/snipeit/
+tmpinstall=/tmp/snipeit/install/
+islog="false"
+log="/var/log/snipeit-install.log"
+if [ -f "$log" ];then
+    echo "log is already here: $log"
+    islog="true"
+fi
+
+
+rm -rf "${tmp:?}"
+mkdir -p "$tmpinstall"
+
+wget "$link"/snipeit.sh -P "$tmpinstall" 2>&1 | grep -i "failed\|error"
+wget "$link"/upgrade.sh -P "$tmpinstall" 2>&1 | grep -i "failed\|error"
+wget "$link"/functions.sh -P "$tmpinstall" 2>&1 | grep -i "failed\|error"
+read test
+chmod -R 755 "$tmpinstall"
+. "$tmpinstall"/snipeit.sh 2>&1 | sudo tee -a /var/log/snipeit-install.log
